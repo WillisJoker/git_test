49a4102d2b33030f218af6001e23bf1dfa317a8d
will <will901024@gmail.com>
2022-07-06 11:35:54 +0800

    today work

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-print("hi")
-print("hi")
-print("hi")
-<<<<<<< HEAD
-=======
-print("hi")
-print("hi")
-print("hissdasdsa")
-print("hi")
->>>>>>> 84e92315adc5dd654de179af5285ad1ebb421fa3
+print("fuck")
--- test.py
+++ test.py
@@ -1,1 +1,5 @@
 print("hello 2sadascas")
+print("hello 2sadascas")
+print("hello 2sadascas")
+print("hello 2sadascas")
+print("hello 2sadascas")
